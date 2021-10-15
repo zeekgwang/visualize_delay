08cfb6a57e024b5cda0da5065f5de0dccb2c3046
zeekgwang <zeekgwang@gmail.com>
2021-10-15 23:20:26 +0900

    feat : increase/decrease the time with button or slider

--- js/animation.js
+++ js/animation.js
@@ -7,6 +7,7 @@ var STATUSDICT = {0 : "RUN", 1 : "PAUSE"}
 
 var mInterval
 var time = 0
+var maxTime = 30
 var intervalSpeed = 1
 var intervalStatus = PAUSE
 var intervalStatusStr = "PAUSE"
@@ -46,10 +47,44 @@ function makeSlower() {
     setIntervalInfo(time, 1 / intervalSpeed, intervalStatusStr)
 }
 
+function prev(){
+    if(intervalStatus === RUN || time <= 0){
+        return
+    }
+
+    time -= 1
+    update(time)
+    draw()
+    setIntervalInfo(time, 1 / intervalSpeed, intervalStatusStr)
+}
+
+function next(){
+    if(intervalStatus === RUN || time >= maxTime){
+        return
+    }
+
+    time += 1
+    update(time)
+    draw()
+    setIntervalInfo(time, 1 / intervalSpeed, intervalStatusStr)
+}
+
+function moveTimeSlider(){
+    if(intervalStatus === RUN){
+        return
+    }
+
+    time = Number(document.getElementById("timeSlider").value)
+    update(time)
+    draw()
+    setIntervalInfo(time, 1 / intervalSpeed, intervalStatusStr)
+}
+
 function setIntervalInfo(time, speed, status){
     document.getElementById("time").textContent = time
     document.getElementById("speed").textContent = speed
     document.getElementById("status").textContent = status
+    document.getElementById("timeSlider").value = time
 }
 
 function run() {
@@ -72,3 +107,7 @@ document.getElementById("startBtn").onclick = start
 document.getElementById("pauseBtn").onclick = pause
 document.getElementById("fasterBtn").onclick = makeFaster
 document.getElementById("slowerBtn").onclick = makeSlower
+document.getElementById("prevBtn").onclick = prev
+document.getElementById("nextBtn").onclick = next
+document.getElementById("timeSlider").onchange = moveTimeSlider
+
